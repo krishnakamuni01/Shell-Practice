5b60159a273f3599c25dc839d86445f31b63068e
krishna <krishnakamuni7@gmail.com>
2026-04-07 14:04:38 +0530

    krish.sh

--- krish.sh
+++ krish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo " sarath hi "
-echo " hi krishna "
-echo " how are you "
-echo " ok "
+
+Person1 = $1
+person2 = $2
+
+echo " $Person1 hi "
+echo " hi $person2 "
+echo " how krishna you "
+echo " ok sarath"
